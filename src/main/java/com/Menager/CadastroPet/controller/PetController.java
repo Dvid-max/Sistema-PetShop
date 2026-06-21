a758d4561f6fd73281ab42988f3db871c9e5b810
David Alves <davidbritoal7@gmail.com>
2026-06-21 19:00:01 -0300

    feat: configura banco de dados

--- src/main/java/com/Menager/CadastroPet/controller/PetController.java
+++ src/main/java/com/Menager/CadastroPet/controller/PetController.java
@@ -1,5 +1,31 @@
 package com.Menager.CadastroPet.controller;
 
+import com.Menager.CadastroPet.model.PetModel;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/animais")
 public class PetController {
 
+    @PostMapping
+    public PetModel cadastrarAnimal(@RequestBody PetModel petModel) {
+        return petModel;
+    }
+
+    @GetMapping
+    public String listarAnimais() {
+        return "Lista";
+    }
+
+    @PutMapping("/{id}")
+    public PetModel atualizarPet(@PathVariable Long id, @RequestBody PetModel petModel) {
+        return  petModel;
+    }
+
+    @DeleteMapping("/{id}")
+    public void  deletarAnimal(@PathVariable Long id) {
+        System.out.println("Animal deletado");
+    }
 }
